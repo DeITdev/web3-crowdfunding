1d0405155416a9ec62a2c84a65581aaa249dbda2
DeITdev <danarikram@gmail.com>
2025-10-16 04:50:00 +0700

    Initial commit: Web3 Crowdfunding Platform with Sepolia support

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+  define: {
+    "process.env": {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      external: ['@assemblyscript/loader'],
+    },
+  },
+});
